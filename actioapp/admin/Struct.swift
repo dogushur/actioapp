b7055c7a6c684bf20a98d700e73afb5c070ff93b
Dogus Hur <appdgshur@gmail.com>
2021-11-15 22:54:55 +0300

    actioapp

---
+++ actioapp/admin/Struct.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+struct SabitDegerler {
+    static var pink = UIColor("#FF1544")
+    static var yellow = UIColor("#F7BD10")
+    static var menuVC: MenuController = MenuController()
+}
